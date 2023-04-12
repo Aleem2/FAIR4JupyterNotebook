fa76b2a4b06354fe658076855f3efcfc1f594cdc
Aleem2 <43530611+Aleem2@users.noreply.github.com>
2023-04-13 00:02:23 +1000

    Create workflow.mk

---
+++ workflow.mk
@@ -0,0 +1,13 @@
+### FAIR for JN Workflow
+
+Create a github repo with license
+Create CITATION.cff and update details
+Upload Jupyter notebook file
+Follow https://mybinder.readthedocs.io/en/latest/howto/languages.html to create requirements.yml file
+Create binder badge following link, https://mybinder.readthedocs.io/en/latest/introduction.html
+Create Google colab badge following instructions https://colab.research.google.com/github/googlecolab/colabtools/blob/master/notebooks/colab-github-demo.ipynb#scrollTo=-pVhOfzLx9us
+Or update the link [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/Aleem2/fair4jndemo/blob/main/boxplot_vs_violin.ipynb)
+And copy it to the readme file.
+Login to Zendo and click on the link to github. 
+Toggle the switch to the link repository. 
+Publish the repository to get DOI.
